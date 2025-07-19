ea0a120ad8c541394977b04ac9d75a8b3651ab2f
Anna Parilova <187332467+ssendns@users.noreply.github.com>
2025-07-19 18:04:09 +0300

    fonts added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 export default function App() {
   return (
-    <div className="text-2xl font-bold text-blue-500 bg-red-500">test</div>
+    <div className="font-poppins text-2xl font-bold text-blue-500 bg-red-500">
+      test
+    </div>
   );
 }
