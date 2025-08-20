d2e7e49cbc09cd5d75d43065e3fc43b0f5837b04
Anna Parilova <187332467+ssendns@users.noreply.github.com>
2025-08-20 17:32:10 +0200

    component name changed

--- frontend/src/RouterWrapper.jsx
+++ frontend/src/RouterWrapper.jsx
@@ -2,15 +2,15 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LogInPage from "./pages/LogInPage";
 import SignUpPage from "./pages/SignUpPage";
 import MainPage from "./pages/MainPage";
-import EditAccountPage from "./pages/EditAccountPage";
+import EditPage from "./pages/EditPage";
 
 export default function RouterWrapper() {
   const routes = [
     { path: "/", element: <MainPage /> },
     { path: "/sign-up", element: <SignUpPage /> },
     { path: "/log-in", element: <LogInPage /> },
-    { path: "/edit", element: <EditAccountPage /> },
-    { path: "/chats/:chatId/edit", element: <EditAccountPage /> },
+    { path: "/edit", element: <EditPage /> },
+    { path: "/chats/:chatId/edit", element: <EditPage /> },
   ];
   const router = createBrowserRouter(routes);
   return <RouterProvider router={router} />;
